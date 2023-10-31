322ab6836072f97a226bb7e7cae2f862ce195bae
Carlos I. Pérez Sechi <secci.jr@gmail.com>
2023-10-31 06:02:55 +0100

    Trying to debug Decimal invalid opration

--- cgt_seccijr/util.py
+++ cgt_seccijr/util.py
@@ -4,4 +4,7 @@ from decimal import *
 def float_round_to_zero(x):
     precision = getcontext().prec
     lowest_num = Decimal(f"1e-{precision}")
-    return float(x) if abs(x) > lowest_num else 0
+    if abs(x) > lowest_num:
+        return float(x)
+    else:
+        return 0
