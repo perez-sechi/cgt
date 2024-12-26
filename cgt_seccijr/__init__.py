1120c7631b593df51a3868e44895309caaa55d0f
Carlos I. Pérez Sechi <secci.jr@gmail.com>
2024-12-26 11:33:53 +0100

    Updating visibility

---
+++ cgt_seccijr/__init__.py
@@ -0,0 +1,5 @@
+from cgt_seccijr.compute import *
+from cgt_seccijr.exploration import *
+from cgt_seccijr.manipulation import *
+from cgt_seccijr.modeling import *
+from cgt_seccijr.visualization import *
--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 setup(
     name='cgt_seccijr',
-    version='0.0.2',
+    version='0.0.3',
     description='Cooperative Game Theory Visualization Tools',
     url='#',
     author='Carlos I. Pérez-Sechi',
