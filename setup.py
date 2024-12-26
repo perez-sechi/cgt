5a7e90ae176f722f68172397965a3be4157beaca
Carlos I. Pérez Sechi <secci.jr@gmail.com>
2024-12-26 12:16:39 +0100

    Adding release url

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     version='0.0.3',
     description='Cooperative Game Theory Visualization Tools',
     url = 'https://github.com/seccijr/cgt',
-    download_url='',
+    download_url='https://github.com/seccijr/cgt/archive/refs/tags/0.0.3.tar.gz',
     author='Carlos I. Pérez-Sechi',
     author_email='secci.jr@gmail.com',
     license='MIT',
