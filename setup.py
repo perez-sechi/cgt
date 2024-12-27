122b5dde3837d59f3dbc5479ceca83147f95ddf0
Carlos I. Pérez Sechi <secci.jr@gmail.com>
2024-12-27 07:07:01 +0100

    Renaming download url

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     version='0.0.4',
     description='Cooperative Game Theory Visualization Tools',
     url = 'https://github.com/seccijr/cgt',
-    download_url='https://github.com/seccijr/cgt/archive/refs/tags/0.0.4.tar.gz',
+    download_url='https://github.com/seccijr/cgt/archive/refs/tags/0.0.6.tar.gz',
     author='Carlos I. Pérez-Sechi',
     author_email='secci.jr@gmail.com',
     license='MIT',
